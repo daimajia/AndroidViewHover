38b03be09b7b595e670de7aeba373b8f60214a37
unknown <318442028@qq.com>
2016-12-18 22:16:50 +0800

    Shrink bitmap before blur,then zoom bitmap after blured

--- library/src/main/java/com/daimajia/androidviewhover/tools/Blur.java
+++ library/src/main/java/com/daimajia/androidviewhover/tools/Blur.java
@@ -1,6 +1,8 @@
 package com.daimajia.androidviewhover.tools;
+
 import android.graphics.Bitmap;
 import android.content.Context;
+import android.graphics.Matrix;
 import android.support.v8.renderscript.Allocation;
 import android.support.v8.renderscript.Element;
 import android.support.v8.renderscript.RenderScript;
@@ -10,12 +12,15 @@ import android.support.v8.renderscript.ScriptIntrinsicBlur;
 public class Blur {
 
     private static final int DEFAULT_BLUR_RADIUS = 10;
+    private static final float DEFAULT_SCALE = 2;
 
     public static Bitmap apply(Context context, Bitmap sentBitmap) {
-        return apply(context, sentBitmap, DEFAULT_BLUR_RADIUS);
+        return apply(context, sentBitmap, DEFAULT_BLUR_RADIUS, DEFAULT_SCALE);
     }
 
-    public static Bitmap apply(Context context, Bitmap sentBitmap, int radius) {
+    public static Bitmap apply(Context context, Bitmap sentBitmap, int radius, float scaleRadius) {
+        // shrink half
+        sentBitmap = scaleBitmap(sentBitmap, 1 / scaleRadius);
         final Bitmap bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
         final RenderScript rs = RenderScript.create(context);
         final Allocation input = Allocation.createFromBitmap(rs, sentBitmap, Allocation.MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
@@ -26,13 +31,21 @@ public class Blur {
         script.setInput(input);
         script.forEach(output);
         output.copyTo(bitmap);
-
+        //zoom bitmap
+        final Bitmap zoomBitmap = scaleBitmap(bitmap, scaleRadius);
+        bitmap.recycle();
         sentBitmap.recycle();
         rs.destroy();
         input.destroy();
         output.destroy();
         script.destroy();
 
-        return bitmap;
+        return zoomBitmap;
+    }
+
+    public static Bitmap scaleBitmap(Bitmap oldBitmap, float scale) {
+        Matrix matrix = new Matrix();
+        matrix.postScale(scale, scale);
+        return Bitmap.createBitmap(oldBitmap, 0, 0, oldBitmap.getWidth(), oldBitmap.getHeight(), matrix, true);
     }
 }
